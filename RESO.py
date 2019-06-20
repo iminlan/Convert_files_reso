30898a6bdddc3aae31794c2e05ad1da39dcf1e4c
iminlan <38048764+iminlan@users.noreply.github.com>
2019-06-20 13:26:20 +0500

    Update RESO.py
    очистка папок для дальнейшего использования

--- RESO.py
+++ RESO.py
@@ -1,5 +1,5 @@
-from os import chdir
-from shutil import move
+from os import listdir, chdir, path, remove
+from shutil import move, rmtree
 import RESO_attachment
 import RESO_detachment
 
@@ -8,3 +8,23 @@ chdir("C:\\Users\\baa\\PycharmProjects\\convert_lists_from_insurance\\DOCX")
 # копируем файлы в папку DOCX
 move('Прикрепить\\RESO_.xls', 'RESO_.xls')
 move('Закрыть\\DETACH_reso_.xls', 'DETACH_reso_.xls')
+
+# почистили папки для дальнейшего использования
+
+dir = "C:\\Users\\baa\\PycharmProjects\\convert_lists_from_insurance\\DOCX\\Прикрепить"
+
+for name in listdir(dir):
+    filepath = path.join(dir, name)
+    try:
+        rmtree(filepath)
+    except OSError:
+        remove(filepath)
+
+dir = "C:\\Users\\baa\\PycharmProjects\\convert_lists_from_insurance\\DOCX\\Закрыть"
+
+for name in listdir(dir):
+    filepath = path.join(dir, name)
+    try:
+        rmtree(filepath)
+    except OSError:
+        remove(filepath)
